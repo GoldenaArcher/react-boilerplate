8e564a3eb7673d1d6531d0f7494b513ffbd840f7
GoldenaArcher <lu.han9467@gmail.com>
2022-04-07 15:18:48 -0600

    add babel, webpack, and scss

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+
+import "./index.scss";
+
+const container = document.getElementById("root");
+
+console.log(container);
+
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: "defaults",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new ESLintPlugin(options)],
+};
